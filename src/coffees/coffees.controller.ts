8b829baebc4890f1228925263c8deff722bef538
fassihhaider <fassihhaider@punch.cool>
2022-06-14 21:16:37 +0500

    feat(coffees): implement CRUD in coffeeService

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,11 +1,11 @@
 import {
-  Controller,
-  Get,
-  Post,
   Body,
-  Patch,
-  Param,
+  Controller,
   Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
 } from '@nestjs/common';
 import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,26 +1,54 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
+import { Coffee } from './entities/coffee.entity';
 
 @Injectable()
 export class CoffeesService {
+  constructor(
+    @InjectRepository(Coffee)
+    private readonly coffeeRepository: Repository<Coffee>,
+  ) {}
+
+  throwCoffeeNotFoundException(coffee: Coffee, id: number) {
+    if (!coffee) {
+      throw new NotFoundException(`Coffee of id: ${id} not found`);
+    }
+  }
+
   create(createCoffeeDto: CreateCoffeeDto) {
-    return 'This action adds a new coffee';
+    const coffee = this.coffeeRepository.create(createCoffeeDto);
+    return this.coffeeRepository.save(coffee);
   }
 
   findAll() {
-    return `This action returns all coffees`;
+    return this.coffeeRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} coffee`;
+  async findOne(id: number) {
+    const coffee = await this.coffeeRepository.findOne({ where: { id } });
+    this.throwCoffeeNotFoundException(coffee, id);
+    return coffee;
   }
 
-  update(id: number, updateCoffeeDto: UpdateCoffeeDto) {
-    return `This action updates a #${id} coffee`;
+  async update(id: number, updateCoffeeDto: UpdateCoffeeDto) {
+    /**
+     * Sees if entity exists or not. If so, returns it with
+     * the values passed. Otherwise, returns undefined
+     */
+    const coffee = await this.coffeeRepository.preload({
+      id,
+      ...updateCoffeeDto,
+    });
+    this.throwCoffeeNotFoundException(coffee, id);
+    return this.coffeeRepository.save(coffee);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} coffee`;
+  async remove(id: number) {
+    const coffee = await this.coffeeRepository.findOne({ where: { id } });
+    this.throwCoffeeNotFoundException(coffee, id);
+    return this.coffeeRepository.remove(coffee);
   }
 }
